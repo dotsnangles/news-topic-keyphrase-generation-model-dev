e6b64339eddd30225137f5aaeb4d54e77d875518
Jeonghyeon Park <dotsnangles@gmail.com>
2023-04-20 18:05:42 +0900

    ADD: f1_score_at_k_for_sample v2, batch inference on gpu

--- src/metrics.py
+++ src/metrics.py
@@ -28,36 +28,46 @@ def rouge_for_batch(labels, predictions):
     return rouge_scores
 
 
-def f1_score_at_k_for_sample(label, prediction, k):
+def f1_score_at_k_for_sample(label_str, prediction_str, k):
     true_positives = 0
     false_positives = 0
     false_negatives = 0
 
     # convert label and prediction strings to sets of key-phrases
     label_lst = [
-        key_phrase.strip() for key_phrase in label.split(";") if key_phrase != ""
+        key_phrase.strip() for key_phrase in label_str.split(";") if key_phrase != ""
     ]
     label_lst = [key_phrase for key_phrase in label_lst if key_phrase != ""]
-    label_set = set(label_lst)
 
     # split the predicted key-phrases and their scores
     prediction_lst = [
-        key_phrase.strip() for key_phrase in prediction.split(";") if key_phrase != ""
+        key_phrase.strip()
+        for key_phrase in prediction_str.split(";")
+        if key_phrase != ""
     ]
     prediction_lst = [key_phrase for key_phrase in prediction_lst if key_phrase != ""][
         :k
     ]
-    prediction_set = set(prediction_lst)
 
     # calculate true positives, false positives, and false negatives
-    for keyphrase in prediction_set:
-        if keyphrase in label_set:
+    for keyphrase in prediction_lst:
+        similarity = False
+        for label in label_lst:
+            if keyphrase in label or label in keyphrase:
+                similarity = True
+                break
+        if similarity == True:
             true_positives += 1
         else:
             false_positives += 1
 
-    for keyphrase in label_set:
-        if keyphrase not in prediction_set:
+    for label in label_lst:
+        similarity = False
+        for keyphrase in prediction_lst:
+            if label in keyphrase or keyphrase in label:
+                similarity = True
+                break
+        if similarity == False:
             false_negatives += 1
 
     # calculate precision, recall, and F1 score
