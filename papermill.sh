36905f7d40451ee578ade6cb166d32412ac8294d
Jeonghyeon Park <dotsnangles@gmail.com>
2023-04-26 13:39:41 +0900

    UPDATE: PEFT model saving and loading tested

--- papermill.sh
+++ papermill.sh
@@ -1,1 +1,1 @@
-papermill ./train_seq2seq_plm.ipynb ./papermill/paust_pko_t5_base_v3_run_8_adafactor_lora_100_epochs.ipynb
+papermill ./train_seq2seq_plm.ipynb ./papermill/paust_pko_t5_base_v3_run_9_adafactor_lora_10_epochs_saving_adapters_test.ipynb
