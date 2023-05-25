6da118312d7b5b469c9938db700e9c80d8bddb1f
Jeonghyeon Park <dotsnangles@gmail.com>
2023-05-25 10:05:04 +0900

    ADD: accelerate config in project

--- train.sh
+++ train.sh
@@ -1,7 +1,6 @@
 # for int8 and peft training
-CUDA_VISIBLE_DEVICES=0 python train.py
+# nohup CUDA_VISIBLE_DEVICES=0 python train.py > /dev/null 2>&1&
 
 # for peft and deepspeed(ZeRO) training
-# accelerate config
-# accelerate launch train.py
-
+# accelerate config --config_file ./conf/accelerate.yaml
+# nohup accelerate launch train.py > /dev/null 2>&1&
